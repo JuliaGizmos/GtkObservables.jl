3d9ed5421975594dfc4e598a4ccc4ad17f0247bc
Tim Holy <tim.holy@gmail.com>
2021-10-09 04:08:32 -0500

    Install artifacts, push preview

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,11 @@
-using Documenter, GtkObservables
+using Documenter, GtkObservables, TestImages
+testimage("lighthouse")    # ensure all artifacts get downloaded before running tests
 
 makedocs(sitename = "GtkObservables",
-         format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+         format   = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
          pages    = ["index.md", "controls.md", "drawing.md", "zoom_pan.md", "reference.md"]
          )
 
-deploydocs(repo   = "github.com/JuliaGizmos/GtkObservables.jl.git",
+deploydocs(repo         = "github.com/JuliaGizmos/GtkObservables.jl.git",
+           push_preview = true,
            )
